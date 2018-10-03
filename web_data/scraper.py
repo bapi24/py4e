66a9be8ae8f0906ec6f2571b8288a92cd113b8c7
Bapi Raj Loya <bapi.loya@gmail.com>
2018-10-03 08:39:13 -0700

    web scraping

---
+++ web_data/scraper.py
@@ -0,0 +1,22 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# start
+url = 'http://py4e-data.dr-chuck.net/known_by_Keiren.html'
+
+count = int(input("Enter count: "))
+position = int(input("Enter position: "))
+
+while count>=0:
+    # get html data
+    print(url)
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    tags = soup('a')
+    url = tags[position - 1].get('href', None)
+    count = count - 1
+   
+     
+    
---
+++ web_data/solution.py
@@ -0,0 +1,23 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# get html data
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# retrieve all anchor tags
+tags = soup('a')
+atags = []
+# print(tags)
+for tag in tags:
+    # print(tag.get('href', None))
+    atags.append(tag.get('href', None))
+
+print(atags)
